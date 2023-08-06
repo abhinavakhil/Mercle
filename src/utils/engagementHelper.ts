c7a59cceb61abcce5104cf22eedc4c62322078a3
Abhinav Akhil <abhinavakhil55@gmail.com>
2023-08-06 19:53:09 +0530

    init

--- src/utils/engagementHelper.ts
+++ src/utils/engagementHelper.ts
@@ -8,27 +8,55 @@ export function engagementMessageOverTimeChartOptions(
 ): Highcharts.Options {
   /**
    * @description
-   * This function combines message counts &
-   * channels from messageCountList and channels,
-   * resulting in a filteredList that includes
-   * valid message counts along with associated channels
+   * Grouping messages by their channel and creating a list
+   * where each channel has an array of its associated messages.
    */
-  const filteredList = messageCountList
-    .map((messageCount) => {
-      const matchingChannel = channels.find(
-        (channel) => channel.id === messageCount.channelId
-      );
+  const messagesList = messageCountList.reduce(
+    (acc: any, message: any): any => {
+      const channelId = message.channelId;
 
-      if (matchingChannel) {
-        return {
-          ...messageCount,
-          channel: matchingChannel,
-        };
+      if (!acc[channelId]) {
+        acc[channelId] = [];
       }
 
-      return null;
-    })
-    .filter(Boolean);
+      acc[channelId].push(message);
+      return acc;
+    },
+    {}
+  );
+
+  /**
+   * @description
+   * Filtering and selecting channels from a list
+   * where the channels have more than one associated message.
+   */
+  const filterChannels = channels.filter(
+    (channel: Channel) => messagesList[channel?.id]?.length > 1
+  );
+
+  /**
+   * @description
+   * Preparing data for creating a chart series, where each series
+   * represents a channel and contains a list of points (x, y) for
+   * displaying message counts over time.
+   */
+  const seriesData = filterChannels.map((channel) => {
+    const channelId = channel?.id;
+    const chartData = messagesList[channelId].map((item: any) => {
+      console.log(item);
+      return {
+        x: new Date(item.timeBucket),
+        y: parseInt(item.count),
+      };
+    });
+
+    console.log(chartData);
+
+    return {
+      name: channel?.name,
+      data: chartData,
+    };
+  });
 
   /**
    * @description
@@ -74,6 +102,7 @@ export function engagementMessageOverTimeChartOptions(
       },
       tickColor: "#5d636d",
       lineColor: "#5d636d",
+      tickWidth: 1,
     },
     plotOptions: {
       series: {
@@ -104,17 +133,6 @@ export function engagementMessageOverTimeChartOptions(
         color: "white",
       },
     },
-    series: [
-      {
-        name: "General",
-        type: "spline",
-        data: filteredList.map((item: any) => {
-          return {
-            x: new Date(item.timeBucket),
-            y: +item.count,
-          };
-        }) as any,
-      },
-    ],
+    series: seriesData as any,
   };
 }
